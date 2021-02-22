1798d371b426caad03cd7355eeadd21bc4db7da3
rohit.r <rajeshrohit031@gmail.com>
2021-02-22 17:28:01 +0530

    Files added

--- Q2.java
+++ Q2.java
@@ -16,15 +16,84 @@ public class Q2 {
 		{
 			a[i]=sc.nextInt();
 		}
-		Pair p1=get(a,n);
-		
+		System.out.println("Linear Method");
+		Pair p1=get(a,n);		
 		System.out.println(" Minimum element is "+ p1.min +" Maximum element is "+ p1.max);
 		System.out.println("Less number of comparisons ");
+		System.out.println("Number of comparisons are 2*(n-2)+1");
+		System.out.println("Divide and Conquer method");
 		Pair p=getmaxmin(a,0,n-1);
 		System.out.print("Minimum element is "+ p.min);
 		System.out.print("Maximum element is "+p.max);
+		System.out.println("Number of comparisons are 3*(n-2)/2 in Even number of elements adn 3*(n-1)/2 in add number of elements");
+		System.out.println("Pairwise comparison ");
+		Pair p2=getmaxmininpair(a,n);
+		System.out.println("Minimum element is "+ p2.min);
+		System.out.println("Maximum element is "+p2.max);
+		System.out.println("Number of comparisons are 3*(n-2)/2 in Even number of elements adn 3*(n-1)/2 in add number of elements");
 	}
-	 private static Pair get(int[] a,int n) {
+	 private static Pair getmaxmininpair(int[] a, int n) {
+		// TODO Auto-generated method stub
+		 Pair p=new Pair();
+		 int min,max,i=0;
+		 if(n==1)
+		 {
+			 p.max=a[0];
+			 p.min=a[0];
+			 return p;
+		 }
+		 if(n%2==0)
+		 {
+			 if(a[0]>a[1])
+			 {
+				 min=a[1];
+				 max=a[0];
+			 }
+			 else
+			 {
+				 min=a[0];
+				 max=a[1];
+			 }
+			 i=2;
+		 }
+		 else
+		 {
+			 min=a[0];
+			 max=a[0];
+			 i=1;
+		 }
+		 while(i<n-1)
+		 {
+			 if(a[i]<a[i+1])
+			 {
+				if(a[i+1]>max)
+				{
+					max=a[i+1];
+				}
+				if(a[i]<min)
+				{
+					min=a[i];
+				}
+			 }
+			 else
+			 {
+				 if(a[i]>max)
+				{
+					max=a[i];
+				}
+				if(a[i+1]<min)
+				{
+					min=a[i+1];
+				}
+				 
+			 }
+			 i++;
+		 }
+		 p.min=min;
+		 p.max=max;
+		return p;
+	}
+	private static Pair get(int[] a,int n) {
 		// TODO Auto-generated method stub
 		 Pair p=new Pair();
 		 Integer max=a[0],min=a[0];
--- Q3.java
+++ Q3.java
@@ -18,20 +18,121 @@ public class Q3 {
 		}
 		int kmin=sc.nextInt();
 		int kmax=sc.nextInt();
-		pair p=usingpredefinedfunction(a,n,kmin,kmax);
-		System.out.println(kmin +" minimum is "+p.min+" and "+ kmax +" maximum is " + p.max);
 		
+		System.out.println("Using 1st Method");
+		  pair p=usingpredefinedfunction(a,n,kmin,kmax); 
+		  System.out.println(kmin +" minimum is "+p.min+" and "+ kmax +" maximum is " + p.max);
+		  System.out.println("Time complexity is O(nlongn)");
+		 
+		System.out.println("Using 2nd Method");
 		pair p1=heapmethod(a,n,kmin,kmax);
-		System.out.println(kmin +" minimum is "+p.min+" and "+ kmax +" maximum is " + p.max);
+		System.out.println(kmin +" minimum is "+p1.min+" and "+ kmax +" maximum is " + p1.max);
+		System.out.println("Time complexity is O(n+klongn)");
+		System.out.println("If n-k+1 <k then we can use min heap for finding kth maximum element");
+		
+		System.out.println("Uisng 3 Method");
+		pair p2=quickselectalgorithm(a,n,kmin,kmax);
+		System.out.println(kmin +" minimum is "+p2.min+" and "+ kmax +" maximum is " + p2.max);
+		System.out.println("Time complexity in best case is O(N)");
 		
 
 	}
 
 	
+	private static pair quickselectalgorithm(int[] a, int n, int kmin, int kmax) {
+		// TODO Auto-generated method stub
+		pair p=new pair();
+		int max=kthmaxpartition(a,0,n-1,kmax);
+		p.max=max;
+		p.min=0;
+		return p;
+	}
+
+
+	private static int kthmaxpartition(int[] a, int l, int h, int k) {
+		// TODO Auto-generated method stub
+		if(k>0 && k<=h-l+1)
+		{
+			int pos=partition(a,l,h);
+			if(pos-l+1==k)
+			{
+				return a[pos];
+			}
+			else if(pos-l+1>k)
+			{
+				return kthmaxpartition(a,l,pos-1,k);
+			}
+			return kthmaxpartition(a,pos+1,h,k-(pos-l+1));
+		}
+		return Integer.MAX_VALUE;
+	}
+
+
+	private static int partition(int[] a, int l, int h) {
+		// TODO Auto-generated method stub
+		int pivot=h;
+		int i=l,j=l;
+		while(j<=h-1)
+		{
+			if(a[j]>=a[pivot])
+			{
+				swap(a,i,j);
+				i++;
+				
+			}
+			j++;
+		}
+		swap(a,i,pivot);
+		int pos=i;
+		return pos;
+	}
+
+
+	private static void swap(int[] a, int i, int j) {
+		// TODO Auto-generated method stub
+		int temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+		
+	}
+
+
 	private static pair heapmethod(int[] a, int n, int kmin, int kmax) {
 		// TODO Auto-generated method stub
 		pair p=new pair();
-		PriorityQueue<Integer> q=new PriorityQueue<Integer>();
+		int max=0,min=0;
+		PriorityQueue<Integer> qmax=new PriorityQueue<Integer>(Collections.reverseOrder());
+		PriorityQueue<Integer> qmin=new PriorityQueue<Integer>();
+
+		for(int i=0;i<n;i++) 
+		{
+			qmax.add(a[i]);
+			qmin.add(a[i]);
+		}
+		int i=1,j=1;
+		while(!qmax.isEmpty())
+		{
+			if(i==kmax)
+			{
+				max=qmax.peek();
+				break;
+			}
+			i++;
+			qmax.poll();
+		}
+		while(!qmin.isEmpty())
+		{
+			if(j==kmin)
+			{
+				min=qmin.peek();
+				break;
+			}
+			j++;
+			qmin.poll();
+		}
+		
+		p.max=max;
+		p.min=min;
 		return p;
 	}
 
